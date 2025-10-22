971c8ec6a0dba629daad6846cbe3ed4cdc1d3f71
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2025-10-22 18:27:02 +0500

    Main celery

--- main/celery.py
+++ main/celery.py
@@ -1,13 +1,22 @@
+# main/celery.py
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
-app = Celery("main")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app = Celery(
-    "main",
-    broker="redis://127.0.0.1:6379/0",
-    backend="redis://127.0.0.1:6379/0",
-)
+app = Celery('main')
+
+# Redis broker va RabbitMQ backend ishlatish uchun:
+app.conf.broker_url = 'amqp://guest:guest@rabbitmq:5672//'
+app.conf.result_backend = 'redis://redis:6379/0'
+
+# Django settings.py ichidan CELERY konfiguratsiyasini olish
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Barcha app’larning tasks.py fayllarini avtomatik yuklash
 app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
