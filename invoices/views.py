04fa30fd314414d6b4afd018ec1266345b92c991
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2025-11-02 17:43:57 +0500

    Invoices views

--- invoices/views.py
+++ invoices/views.py
@@ -1,47 +1,51 @@
+# invoices/views.py
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, permissions, status, filters
+from rest_framework import generics, status, filters, permissions
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
 from .models import Invoice
 from .serializers import InvoiceSerializer
 from .message import get_message
-from custom_permissions.permission import *
-from rest_framework.pagination import PageNumberPagination
+from custom_permissions.permission import IsOwnerOrAdmin
 
-# New
+
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 1
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 50
-# New version
+
+
 class InvoiceListCreateView(generics.ListCreateAPIView):
-
+    """
+    - List: authenticated users see their invoices; admin/accountant see all.
+    - Create: any authenticated user can create an invoice (it will be saved with request.user).
+    """
     serializer_class = InvoiceSerializer
-    queryset = Invoice.objects.all()
-    permission_classes = [IsAdminOrAccountant]
     pagination_class = StandardResultsSetPagination
-
+    permission_classes = [permissions.IsAuthenticated]  # require auth to list/create
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['status', 'client_name']
     search_fields = ['client_name']
     ordering_fields = ['amount', 'due_date', 'created_at']
 
+    def get_queryset(self):
+        user = self.request.user
+        if not user or not user.is_authenticated:
+            return Invoice.objects.none()
+
+        if getattr(user, "role", None) in ("admin", "accountant"):
+            return Invoice.objects.all()
+
+        return Invoice.objects.filter(user=user)
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    def get_queryset(self):
-        user = self.request.user
-        if not user.is_authenticated:
-            return Invoice.objects.none()
-        if user.role in ['admin', 'accountant']:
-            return Invoice.objects.all()
-        return Invoice.objects.filter(user=user)
-
-
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data, context={"request": request})
+        serializer = self.get_serializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-
         return Response(
             {
                 "message": get_message("invoice_created", request.user),
@@ -52,26 +56,28 @@ class InvoiceListCreateView(generics.ListCreateAPIView):
 
 
 class InvoiceDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Detail/update/delete on a single invoice.
+    - Uses object-level permission IsOwnerOrAdmin.
+    - get_queryset restricts list of accessible objects (defensive).
+    """
     serializer_class = InvoiceSerializer
-    # permission_classes = [IsAdminOrAccountant]
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
 
     def get_queryset(self):
         user = self.request.user
-        if not user.is_authenticated:
+        if not user or not user.is_authenticated:
             return Invoice.objects.none()
-        if user.role in ["admin", "accountant"]:
+        if getattr(user, "role", None) in ("admin", "accountant"):
             return Invoice.objects.all()
         return Invoice.objects.filter(user=user)
-        
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop("partial", False)
         instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial, context={"request": request})
+        serializer = self.get_serializer(instance, data=request.data, partial=partial, context={'request': request})
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
-
         return Response(
             {
                 "message": get_message("invoice_updated", request.user),
@@ -82,14 +88,6 @@ class InvoiceDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
+        # IsOwnerOrAdmin already denies if not allowed
         self.perform_destroy(instance)
-
-        return Response(
-            {"message": get_message("invoice_deleted", request.user)},
-            status=status.HTTP_200_OK,
-        )
-# New
-class InvoiceRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Invoice.objects.all()
-    serializer_class = InvoiceSerializer
-    permission_classes = [IsOwnerOrAdmin]
+        return Response({"message": get_message("invoice_deleted", request.user)}, status=status.HTTP_200_OK)
