17ecfc1e36fef6d3f3d60ba6c94bca608dd34f1f
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2025-11-03 18:16:36 +0500

    users urls

--- users/urls_auth.py
+++ users/urls_auth.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import LoginView, RefreshView
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="token_obtain_pair"),
+    path("login/", LoginView.as_view(), name="login"),
     path("refresh/", RefreshView.as_view(), name="token_refresh"),
 ]
