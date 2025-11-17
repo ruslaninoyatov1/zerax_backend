c4cdbae0dee11c8201ebc79e336a2b90596ddb1b
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2025-11-17 14:00:42 +0500

    initial commit

--- main/settings.py
+++ main/settings.py
@@ -88,25 +88,28 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-#
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'zerax_db',
-#         'USER': 'zerax_admin',
-#         'PASSWORD': 'root',
-#         'HOST': 'db',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'zerax_db',
+                'USER': 'zerax_admin',
+                'PASSWORD': 'root',
+                'HOST': 'localhost',
+                'PORT': '5432',
+                'OPTIONS': {
+                    'options': '-c client_encoding=UTF8'
+                },
+            }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
